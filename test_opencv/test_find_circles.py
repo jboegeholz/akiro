92727317d978832895e22fef7b919588a41e6da1
jboegeholz <boegeholz.joern@gmail.com>
2024-09-30 09:32:03 +0200

    add tuning params

--- test_opencv/test_find_circles.py
+++ test_opencv/test_find_circles.py
@@ -27,15 +27,17 @@ def test_convert_br_2_hsv():
     cv2.waitKey(0)
     assert working_image.any()
 
+
 def test_apply_hsv_threshold():
     cv_image = cv2.imread(TEST_IMAGE)
     working_image = cv2.blur(cv_image, (5, 5))
     working_image = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)
-    thresh_min = ()
-    thresh_max = ()
+    thresh_min = (21, 153, 42)
+    thresh_max = (255, 255, 255)
     working_image    = cv2.inRange(working_image, thresh_min, thresh_max)
     cv2.imshow("Blur", working_image)
     cv2.waitKey(0)
     assert working_image.any()
+
 
 
