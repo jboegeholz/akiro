63fac4b2b5cd590f8fd0bc0eacb4ba334677119f
jboegeholz <boegeholz.joern@gmail.com>
2024-09-30 15:06:27 +0200

    add sample code

--- src/ball_tracker/ball_tracker/process_image.py
+++ src/ball_tracker/ball_tracker/process_image.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
 
@@ -14,13 +14,28 @@ class ImageSubscriber(Node):
             '/image_raw',
             self.listener_callback,
             10)
+        self.get_logger().info('<< Subscribed to /image_raw')
         self.bridge = CvBridge()
 
     def listener_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        self.get_logger().info('Receive image data...')
+        cv_image = None
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.get_logger().info('Receive image data...')
+        except CvBridgeError as e:
+            self.get_logger().error("CvBridgeError!")
+            return
 
-        cv2.imshow("Image Window", cv_image)
+        working_image = cv2.blur(cv_image, (5, 5))
+        working_image = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)
+        thresh_min = (21, 153, 42)
+        thresh_max = (255, 255, 255)
+        working_image = cv2.inRange(working_image, thresh_min, thresh_max)
+        working_image = cv2.dilate(working_image, None, iterations=2)
+        working_image = cv2.erode(working_image, None, iterations=2)
+        working_image = 255 - working_image
+
+        cv2.imshow("Image Window", working_image)
         cv2.waitKey(1)
 
 
--- test_opencv/test_find_circles.py
+++ test_opencv/test_find_circles.py
@@ -34,10 +34,61 @@ def test_apply_hsv_threshold():
     working_image = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)
     thresh_min = (21, 153, 42)
     thresh_max = (255, 255, 255)
-    working_image    = cv2.inRange(working_image, thresh_min, thresh_max)
+    working_image = cv2.inRange(working_image, thresh_min, thresh_max)
     cv2.imshow("Blur", working_image)
     cv2.waitKey(0)
     assert working_image.any()
 
 
+def test_dilate_erode():
+    cv_image = cv2.imread(TEST_IMAGE)
+    working_image = cv2.blur(cv_image, (5, 5))
+    working_image = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)
+    thresh_min = (21, 153, 42)
+    thresh_max = (255, 255, 255)
+    working_image = cv2.inRange(working_image, thresh_min, thresh_max)
+    working_image = cv2.dilate(working_image, None, iterations=2)
+    working_image = cv2.erode(working_image, None, iterations=2)
+    working_image = 255 - working_image
+    cv2.imshow("Blur", working_image)
+    cv2.waitKey(0)
+    assert working_image.any()
 
+def test_blob_detector():
+    cv_image = cv2.imread(TEST_IMAGE)
+    working_image = cv2.blur(cv_image, (5, 5))
+    working_image = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)
+    thresh_min = (21, 153, 42)
+    thresh_max = (255, 255, 255)
+    working_image = cv2.inRange(working_image, thresh_min, thresh_max)
+    working_image = cv2.dilate(working_image, None, iterations=2)
+    working_image = cv2.erode(working_image, None, iterations=2)
+    working_image = 255 - working_image
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Change thresholds
+    params.minThreshold = 0
+    params.maxThreshold = 100
+
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 30
+    params.maxArea = 20000
+
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.1
+
+    # Filter by Convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0.5
+
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.5
+
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Run detection!
+    keypoints = detector.detect(working_image)
+    assert keypoints == ()
