63cb1a82f980a65684fcd815cb23961e3266e8a1
jboegeholz <boegeholz.joern@gmail.com>
2024-10-05 10:36:07 +0200

    adjust framerate

--- src/ball_tracker/launch/start_robot.launch.py
+++ src/ball_tracker/launch/start_robot.launch.py
@@ -14,6 +14,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'video_device': '/dev/video0',
+                'time_per_frame': [1, 6],
                 'pixel_format': 'YUYV',
                 'image_size': [640, 480]
             }]
