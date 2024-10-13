44b27511099b3d026e2746b3f371d5d3a41397ef
jboegeholz <boegeholz.joern@gmail.com>
2024-10-13 21:05:49 +0200

    add teleop_twist_joy

--- src/ball_tracker/launch/teleop_twist_keyboard.launch.py
+++ src/ball_tracker/launch/teleop_twist_keyboard.launch.py
@@ -6,22 +6,34 @@ import os
 def generate_launch_description():
     package_path = get_package_share_directory('ball_tracker')
 
-    config_file_path = os.path.join(package_path, 'config', 'twist_mux.yaml')
-    
-    return LaunchDescription([
-        Node(
+    twist_mux_config_file_path = os.path.join(package_path, 'config', 'twist_mux.yaml')
+    twist_joy_config_file_path = os.path.join(package_path, 'config', 'twist_joy.yaml')
+    teleop_twist_key = Node(
             package='teleop_twist_keyboard',
             executable='teleop_twist_keyboard',
             output='screen',
             remappings=[
-                ('/cmd_vel', '/cmd_vel_prio')
+                ('/cmd_vel', '/cmd_vel_sec')
             ]
-        ),
-        Node(
+        )
+    teleop_twist_joy = Node(
+            package='teleop_twist_joy',
+            executable='teleop_twist_joy',
+            output='screen',
+            remappings=[
+                ('/cmd_vel', '/cmd_vel_prio')
+            ],
+            parameters=[twist_joy_config_file_path]
+        )
+    twist_mux = Node(
             package='twist_mux',
             executable='twist_mux',
             name='twist_mux',
             output='screen',
-            parameters=[config_file_path]
+            parameters=[twist_mux_config_file_path]
         )
+    return LaunchDescription([
+        teleop_twist_key,
+        teleop_twist_joy,
+        twist_mux
     ])
