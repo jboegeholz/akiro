c229ea76ac37f060f56b0a9e731ba5be04f1783f
jboegeholz <boegeholz.joern@gmail.com>
2024-11-03 20:52:10 +0100

    rework serial test

--- test_opencv/test_pyserial.py
+++ test_opencv/test_pyserial.py
@@ -1,14 +1,15 @@
 from serial.serialutil import SerialException
 import serial
 
-def test_pyserial():
-
+def test_pyserial_on_mac_os():
     try:
         serial_port = serial.Serial(
-            port='/dev/ttyUSB0',  # Passe den Port an
+            port='/dev/cu.usbserial-110',  # for mac os
             baudrate=9600,
             timeout=1
         )
     except SerialException as e:
-        print(e)
-    assert serial_port
+        serial_port = serial.serial_for_url('loop://', timeout=1)
+
+    serial_port.write("asd".encode("utf-8"))
+    assert serial_port.readline() == b"asd"
