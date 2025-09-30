77e7086a264b8f3a3beb913bf511fa7e6b444d4d
jboegeholz <boegeholz.joern@gmail.com>
2025-09-30 16:17:14 +0200

    rework launch file

---
+++ src/my_first_package/launch/pub_sub.launch.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    package_dir = get_package_share_directory('my_first_package')
+
+    publisher_launch_path = os.path.join(package_dir, 'launch', 'publisher.launch.py')
+    publisher_launch_description = IncludeLaunchDescription(PythonLaunchDescriptionSource(publisher_launch_path))
+
+    subscriber_node = Node(
+            package='demo_nodes_py',
+            executable='listener',
+            name='eary',
+            output='screen'
+        )
+
+    ld.add_action(subscriber_node)
+    ld.add_action(publisher_launch_description)
+    return ld
