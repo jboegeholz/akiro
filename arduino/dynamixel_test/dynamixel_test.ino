cc82103d3f6e21c9fd5b6a03c90f78bd762b583b
jboegeholz <boegeholz.joern@gmail.com>
2025-10-12 20:54:33 +0200

    use both motors

--- arduino/dynamixel_test/dynamixel_test.ino
+++ arduino/dynamixel_test/dynamixel_test.ino
@@ -1,27 +1,40 @@
 #include <DynamixelShield.h> 
-const uint8_t DXL_ID = 1; 
+const uint8_t DXL_ID_1 = 1;
+const uint8_t DXL_ID_2 = 2;
 const uint32_t BAUDRATE = 57600; 
 const float DXL_PROTOCOL_VERSION = 2.0; 
 
-DynamixelShield dxl; void 
+DynamixelShield dxl; 
 
-setup() { 
+void setup() { 
   dxl.begin(BAUDRATE); 
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION); 
 
-  if(dxl.ping(DXL_ID) == true){ 
-    dxl.torqueOff(DXL_ID); 
-    dxl.setOperatingMode(DXL_ID, OP_VELOCITY); 
-    dxl.torqueOn(DXL_ID); 
-    
+  if(dxl.ping(DXL_ID_1) == true){ 
+    dxl.torqueOff(DXL_ID_1); 
+    dxl.setOperatingMode(DXL_ID_1, OP_VELOCITY); 
+    dxl.torqueOn(DXL_ID_1);   
   } 
+  
+  if (dxl.ping(DXL_ID_2)) {
+    dxl.torqueOff(DXL_ID_2);
+    dxl.setOperatingMode(DXL_ID_2, OP_VELOCITY);
+    dxl.torqueOn(DXL_ID_2);
+  }
 
 } 
 
 void loop() { 
-  dxl.setGoalVelocity(DXL_ID, 200.0, UNIT_RAW); 
-  delay(1000); 
-  dxl.setGoalVelocity(DXL_ID, 25.8, UNIT_RPM); 
-  delay(1000); 
-  dxl.setGoalVelocity(DXL_ID, -10.2, UNIT_PERCENT); 
+  dxl.setGoalVelocity(DXL_ID_1, 200.0, UNIT_RAW); 
+  dxl.setGoalVelocity(DXL_ID_2, 200.0, UNIT_RAW); 
+  delay(3000); 
+  dxl.setGoalVelocity(DXL_ID_1, 0.0, UNIT_RAW); 
+  dxl.setGoalVelocity(DXL_ID_2, 0.0, UNIT_RAW); 
+  delay(3000); 
+  dxl.setGoalVelocity(DXL_ID_1, -200.0, UNIT_RAW); 
+  dxl.setGoalVelocity(DXL_ID_2, -200.0, UNIT_RAW); 
+  delay(3000); 
+  dxl.setGoalVelocity(DXL_ID_1, 0.0, UNIT_RAW); 
+  dxl.setGoalVelocity(DXL_ID_2, 0.0, UNIT_RAW); 
+  delay(3000); 
 }
